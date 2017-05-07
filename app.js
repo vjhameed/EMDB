16fcd37a17e483a51058c836a55b2dda3697b487
hameedullah <vjhameed3@gmail.com>
2017-05-07 09:03:51 -0700

    first version

---
+++ app.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Administrator on 5/6/2017.
+ */
+const express = require('express');
+const request = require('request');
+const bodyparser = require('body-parser');
+var app = express();
+
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.set("view engine","ejs");
+app.use(express.static("public"));
+
+app.get("/",(req,res)=>{
+    res.render("search");
+});
+
+app.post("/posted",(req,res)=>{
+    var result;
+        var moviename  =  encodeURIComponent(req.body.mname);
+        var type  =  req.body.type  ? encodeURIComponent(req.body.type) : "movie";
+        var plot  =  req.body.plot ? encodeURIComponent(req.body.plot) : "full";
+        var url = `http://www.omdbapi.com/?s=${moviename}&plot=${plot}&type=${type}`
+        request(url,(error,response,body)=>{
+       // if(response.statusCode == 200 )
+            result = JSON.parse(body);
+            res.render("results",{thingvar:result})
+        });
+});
+
+app.listen(3000,()=>{console.log("server has started") })
